a220bfbe801e4a2f1251704a4a1d1cb8ebef4542
Douglas de Assis da Silva <ddadsx@gmail.com>
2019-10-05 17:53:36 -0400

    Add Base files for mobile in React Native

---
+++ mobile/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <Text>This is my App</Text>
+      
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
