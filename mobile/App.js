5a48701537986a5ccd335dce03468df99f101ce8
Douglas de Assis da Silva <ddadsx@gmail.com>
2019-12-05 15:42:52 -0400

    Add list of spots

--- mobile/App.js
+++ mobile/App.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>This is my App</Text>
-      
-    </View>
-  );
+  return <Routes />
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,1 +1,16 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Login from './pages/Login';
+import List from './pages/List';
+import Book from './pages/Book';
+
+//CreateAppContainer deve estar em volta de todas rotas
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Login,
+        List,
+        Book
+    })
+);
+
+export default Routes;
